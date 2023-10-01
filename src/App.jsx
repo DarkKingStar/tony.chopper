5327c8324e2b8038fb95c18c6ed3b388975c687b
DarkKingStar <sounakguha.12345@gmail.com>
2023-10-01 14:56:42 +0530

    demo initials-7

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import AppRouter from './Approuter';
+import Footer from './assets/components/footer';
 
 function App() {
   return (
     <>
      <AppRouter/>
+     <Footer/>
     </>
   )
 }
---
+++ src/assets/components/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css'
+const Footer = () => {
+    return ( <>
+    <>
+    <div className="footer-container">
+            Copyright &copy; DarkinStar
+    </div>
+    </>
+    </> );
+}
+
+export default Footer;
