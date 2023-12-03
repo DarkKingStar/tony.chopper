2ee9953eb9f1b6860bfe08ac261e91fa1b2b3768
DarkinStar <sounakguha.12345@gmail.com>
2023-12-03 09:43:46 +0530

    add extarnal consumet server2

--- src/assets/fetch/fetchsearchdata.js
+++ src/assets/fetch/fetchsearchdata.js
@@ -1,9 +1,12 @@
 import { ANIME } from '@consumet/extensions';
 export const FetchSearchData = async(query)=>{
     try {
-        const gogoanime = new ANIME.Gogoanime();
-        const response = await gogoanime.search(query);
-        return response;
+        const response = await fetch(`https://backendchopper.onrender.com/search/${query}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        return jsonData;
     } catch (error) {
         console.error('Error fetching data:', error);
         return null; 
